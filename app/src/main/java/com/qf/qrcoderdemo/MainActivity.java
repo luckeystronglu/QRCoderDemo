1dd01e52bab431a6eabc68626c0202fa2342a772
luckeystronglu <zhuoao539176588>
2016-09-02 13:59:07 +0800

    解决生成二维码时，内容为空的问题

--- app/src/main/java/com/qf/qrcoderdemo/MainActivity.java
+++ app/src/main/java/com/qf/qrcoderdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.zxing.activity.CaptureActivity;
 import com.zxing.util.QRcodeUtil;
@@ -25,13 +26,33 @@ public class MainActivity extends AppCompatActivity {
         iv = (ImageView) findViewById(R.id.iv);
     }
 
+    /**
+     * 	String contentString = qrStrEditText.getText()
+     .toString();
+     if (contentString != null && !contentString.equals("")) {
+     Intent getAlbum = new Intent(
+     Intent.ACTION_GET_CONTENT);
+     getAlbum.setType(IMAGE_TYPE);
+     startActivityForResult(getAlbum, IMAGE_CODE);
+     } else {
+     Toast.makeText(getApplication(), "未填写信息",
+     Toast.LENGTH_LONG).show();
+     }
+     * @param v
+     */
     public void btnClick(View v) {
         switch (v.getId()) {
             case R.id.btn1:
                 //生成二维码
                 String str = et.getText().toString();
-                Bitmap bitmap = QRcodeUtil.btnGenRawCode(this, str);
-                iv.setImageBitmap(bitmap);
+                if (str == null || str.equals("")){
+                    Toast.makeText(MainActivity.this, "没有任务内容", Toast.LENGTH_SHORT).show();
+                }else {
+                    Bitmap bitmap = QRcodeUtil.btnGenRawCode(this, str);
+                    iv.setImageBitmap(bitmap);
+
+                }
+
                 break;
             case R.id.btn2:
                 //扫一扫
